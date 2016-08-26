f08ce2115ea08d3876b3d383f62eb90a965bed67
MrNuts <shmel_v@mail.ru>
2016-08-27 01:07:25 +0300

    new

--- js/code.js
+++ js/code.js
@@ -20,7 +20,7 @@ $('.bottom').click(function() {
 	    offset: 100    
     });
     setTimeout(function() {
-        $('.content2').viewportChecker({
+        $('.content-start').viewportChecker({
             classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
             offset: 100    
         });
