d98ad0c7c069e3b9039f47a335c520dfc74bd0b9
MrNuts <shmel_v@mail.ru>
2016-08-28 00:54:26 +0300

    new

--- js/code.js
+++ js/code.js
@@ -1,34 +1,25 @@
-jQuery(document).ready(function() {
-	jQuery('.content').viewportChecker({
-	    classToAdd: 'animated bounceInUp', // Class to add to the elements when they are visible
-	    offset: 100,
-	   });   
-});
-    
-//jQuery(document).ready(function() {
-//    setTimeout(function() {
-//        $('.content').viewportChecker({
-//         classToAdd: 'animated bounceInUp', // Class to add to the elements when they are visible
-//         offset: 100,
-//        }); 
-//    }, 100);
-//});
-    
-$('.bottom').click(function() {
-	$('.content').viewportChecker({
-	    classToAdd: 'animated bounceOutUp', // Class to add to the elements when they are visible
-	    offset: 100,
+jQuery(document).ready(function () {
+    jQuery('.content').viewportChecker({
+        classToAdd: 'animated bounceInUp',
+        offset: 100,
     });
-    setTimeout(function() {
-        $('.content-start').viewportChecker({
-            classToAdd: 'visible animated bounceInUp visible', // Class to add to the elements when they are visible
-            offset: 100    
+});
+
+jQuery('.bottom').click(function () {
+    jQuery('.content').viewportChecker({
+        classToAdd: 'animated bounceOutUp',
+        offset: 100,
+    });
+    setTimeout(function () {
+        jQuery('.content').addClass('pos-a').viewportChecker({
+            classToAdd: 'pos-a',
+            offset: 100
         });
     }, 800);
-    setTimeout(function() {
-        $('.content').viewportChecker({
-            classToAdd: 'pos-a', // Class to add to the elements when they are visible
-            offset: 100    
+    setTimeout(function () {
+        jQuery('.content-start').viewportChecker({
+            classToAdd: 'visible animated bounceInUp visible',
+            offset: 100
         });
-    }, 900);    
-});  
+    }, 800);
+});
